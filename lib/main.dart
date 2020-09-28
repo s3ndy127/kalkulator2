ce7e9a58ace35fd0bb7659249ba3662ea7377f64
s3ndy127 <sendy.saputra10@gmail.com>
2020-09-28 11:27:19 +0700

    test

---
+++ lib/main.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+
+import 'parser.dart';
+
+void main() => runApp(new MyApp());
+
+const appName = 'Flutter Calculator';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: appName,
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.tealAccent,
+      ),
+      home: new Main(),
+    );
+  }
+}
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Column(
+        children: <Widget>[
+          new Display(),
+          new Keyboard(),
+        ],
+      ),
+    );
+  }
+}
+
+var _displayState = new DisplayState();
+
+class Display extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _displayState;
+  }
+}
+
+class DisplayState extends State<Display> {
+  var _expression = '';
+  var _result = '';
+
+  @override
+  Widget build(BuildContext context) {
+    var views = <Widget>[
+      new Expanded(
+          flex: 1,
+          child: new Row(
+            children: <Widget>[
+              new Expanded(
+                  child: new Text(
+                    _expression,
+                    textAlign: TextAlign.right,
+                    style: new TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.white,
+                    ),
+                  ))
+            ],
+          )),
+    ];
+
+    if (_result.isNotEmpty) {
+      views.add(new Expanded(
+          flex: 1,
+          child: new Row(
+            children: <Widget>[
+              new Expanded(
+                  child: new Text(
+                    _result,
+                    textAlign: TextAlign.right,
+                    style: new TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.white,
+                    ),
+                  ))
+            ],
+          )),
+      );
+    }
+
+    return new Expanded(
+        flex: 2,
+        child: new Container(
+          color: Theme
+              .of(context)
+              .primaryColor,
+          padding: const EdgeInsets.all(16.0),
+          child: new Column(
+            children: views,
+          ),
+        ));
+  }
+}
+
+void _addKey(String key) {
+  var _expr = _displayState._expression;
+  var _result = '';
+  if (_displayState._result.isNotEmpty) {
+    _expr = '';
+    _result = '';
+  }
+
+  if (operators.contains(key)) {
+    if (_expr.length > 0 && operators.contains(_expr[_expr.length - 1])) {
+      _expr = _expr.substring(0, _expr.length - 1);
+    }
+    _expr += key;
+  } else if (digits.contains(key)) {
+    _expr += key;
+  } else if (key == 'C') {
+    if (_expr.length > 0) {
+      _expr = _expr.substring(0, _expr.length - 1);
+    }
+  } else if (key == '=') {
+    try {
+      var parser = const Parser();
+      _result = parser.parseExpression(_expr).toString();
+    } on Error {
+      _result = 'Error';
+    }
+  }
+  _displayState.setState(() {
+    _displayState._expression = _expr;
+    _displayState._result = _result;
+  });
+}
+
+class Keyboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Expanded(
+        flex: 4,
+        child: new Center(
+            child:
+            new AspectRatio(
+              aspectRatio: 1.0,
+              child: new GridView.count(
+                crossAxisCount: 4,
+                childAspectRatio: 1.0,
+                padding: const EdgeInsets.all(4.0),
+                mainAxisSpacing: 4.0,
+                crossAxisSpacing: 4.0,
+                children: <String>[
+                  // @formatter:off
+                  '7', '8', '9', '/',
+                  '4', '5', '6', '*',
+                  '1', '2', '3', '-',
+                  'C', '0', '=', '+',
+                  // @formatter:on
+                ].map((key) {
+                  return new GridTile(
+                    child: new KeyboardKey(key),
+                  );
+                }).toList(),
+              ),
+            )
+        ));
+  }
+}
+
+class KeyboardKey extends StatelessWidget {
+  KeyboardKey(this._keyValue);
+
+  final _keyValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return new FlatButton(
+      child: new Text(
+        _keyValue,
+        style: new TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 26.0,
+          color: Colors.black,
+        ),
+      ),
+      color: Theme
+          .of(context)
+          .scaffoldBackgroundColor,
+      onPressed: () {
+        _addKey(_keyValue);
+      },
+    );
+  }
+}
